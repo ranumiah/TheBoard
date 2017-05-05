2ee9a56f4718711933fe3a676c1a746ead631b07
Ranu <O@O.net>
2017-05-05 13:55:06 +0100

    Accessing the MongoDB Server

---
+++ TheBoard/data/database.js
@@ -0,0 +1,26 @@
+﻿// database.js
+(function (database) {
+
+    var mongodb = require("mongodb");
+    var mongoUrl = "mongodb://localhost:27017/theBoard";
+    var theDb = null;
+
+    database.getDb = function (next) {
+        if (!theDb) {
+            // connect to the database
+            mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    theDb = {
+                        db: db
+                    };
+                    next(null, theDb);
+                }
+            });
+        } else {
+            next(null, theDb);
+        }
+    }
+
+})(module.exports);
